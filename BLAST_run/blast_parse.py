fb574a8a43c3c5c442fe3290adeea84d12d65f2a
P Mulhair <bspm@login2.arc3.leeds.ac.uk>
2019-02-26 17:57:35 +0000

    BLAST command line

--- BLAST_run/blast_parse.py
+++ BLAST_run/blast_parse.py
@@ -1,20 +1,24 @@
-#Parse blastoutput files to get hits with greater than 80 coverage
-
+##Parse blastoutput files to get hits with greater than 80 coverage and 30 pident
 taxa_count = 0
 mincover = 80
-blast_hits = []
-with open('../data/results/blast_output/' + k + '_blast.fasta.blastoutput') as f2:
-    for line in f2:
+min_pident = 30
+#Open blast output file, and blast parse file to write to
+with open('file.fasta.blastoutput') as f, open('file.fasta.blastparse', 'w') as outF:
+    for line in f:
         lines = line.split('\t')
         hit_ID = lines[1]
         evalue = lines[2]
+        pident = lines[3]
         query_start = lines[5]
         query_end = lines[6]
         sub_start = lines[-3]
         sub_end = lines[-2]
+        #Calculate covergae from query and subject start and end hits/len of gene
         cover_que = 100 * (int(lines[6])-int(lines[5])) / int(lines[7])
         cover_sub = 100.0 * (int(lines[9])-int(lines[8])) / int(lines[10])
-        wanted_values = (cover_que >= mincover) and (cover_sub > mincover)
+        #Create variable of wanted values for coverage and percent identity
+        wanted_values = (cover_que >= mincover) and (cover_sub > mincover) and (pident >= min_pident)
+        #If wanted values are satisfied, write the BLAST result to file
         if wanted_values:
             taxa_count += 1
-            blast_hits.extend((hit_ID, query_start, query_end, cover_que, cover_sub, '\n'))
+            outF.write(str(hit_ID) + '\t'  + str(query_start) + '\t' + str(query_end) + '\t' + str(sub_start) + '\t' + str(sub_end) + '\t' + str(cover_que) + '\t' + str(cover_sub) + '\n')
