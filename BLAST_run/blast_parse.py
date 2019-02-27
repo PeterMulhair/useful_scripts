1236576de1f3d68b7e0a1036f143eae53e5bad8a
P Mulhair <bspm@login1.arc3.leeds.ac.uk>
2019-02-27 17:48:59 +0000

    BLAST command line

--- BLAST_run/blast_parse.py
+++ BLAST_run/blast_parse.py
@@ -6,6 +6,7 @@ min_pident = 30
 with open('file.fasta.blastoutput') as f, open('file.fasta.blastparse', 'w') as outF:
     for line in f:
         lines = line.split('\t')
+        query_ID = lines[0]
         hit_ID = lines[1]
         evalue = lines[2]
         pident = lines[3]
@@ -17,8 +18,8 @@ with open('file.fasta.blastoutput') as f, open('file.fasta.blastparse', 'w') as 
         cover_que = 100 * (int(lines[6])-int(lines[5])) / int(lines[7])
         cover_sub = 100.0 * (int(lines[9])-int(lines[8])) / int(lines[10])
         #Create variable of wanted values for coverage and percent identity
-        wanted_values = (cover_que >= mincover) and (cover_sub > mincover) and (pident >= min_pident)
+        wanted_values = (cover_que >= mincover) and (cover_sub > mincover) and (float(pident) >= float(min_pident))
         #If wanted values are satisfied, write the BLAST result to file
         if wanted_values:
             taxa_count += 1
-            outF.write(str(hit_ID) + '\t'  + str(query_start) + '\t' + str(query_end) + '\t' + str(sub_start) + '\t' + str(sub_end) + '\t' + str(cover_que) + '\t' + str(cover_sub) + '\n')
+            outF.write(str(query_ID) + '\t' + str(hit_ID) + '\t'  + str(query_start) + '\t' + str(query_end) + '\t' + str(sub_start) + '\t' + str(sub_end) + '\t' + str(cover_que) + '\t' + str(cover_sub) + '\n')
