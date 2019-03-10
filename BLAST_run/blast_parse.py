bd401a03390772b6b484937abb667bd81c8b8011
P Mulhair <bspm@login1.arc3.leeds.ac.uk>
2019-03-10 13:31:59 +0000

    BLAST command line

--- BLAST_run/blast_parse.py
+++ BLAST_run/blast_parse.py
@@ -1,9 +1,22 @@
 ##Parse blastoutput files to get hits with greater than 80 coverage and 30 pident
-taxa_count = 0
+import argparse
+import sys
+
+
+parse = argparse.ArgumentParser()
+
+#Specify input and output files on the command line
+parse.add_argument("--blastout", "-in",type=str,help="name of the blastoutput file to be parsed",required=True)
+parse.add_argument("--blastparse", "-out",type=str,help="name of the blast parsed file to write to",required=True)
+
+args = parse.parse_args()
+
+
+
 mincover = 80
 min_pident = 30
 #Open blast output file, and blast parse file to write to
-with open('file.fasta.blastoutput') as f, open('file.fasta.blastparse', 'w') as outF:
+with open(args.blastout) as f, open(args.blastparse, 'w') as outF:
     for line in f:
         lines = line.split('\t')
         query_ID = lines[0]
@@ -21,5 +34,4 @@ with open('file.fasta.blastoutput') as f, open('file.fasta.blastparse', 'w') as 
         wanted_values = (cover_que >= mincover) and (cover_sub > mincover) and (float(pident) >= float(min_pident))
         #If wanted values are satisfied, write the BLAST result to file
         if wanted_values:
-            taxa_count += 1
             outF.write(str(query_ID) + '\t' + str(hit_ID) + '\t'  + str(query_start) + '\t' + str(query_end) + '\t' + str(sub_start) + '\t' + str(sub_end) + '\t' + str(cover_que) + '\t' + str(cover_sub) + '\n')
